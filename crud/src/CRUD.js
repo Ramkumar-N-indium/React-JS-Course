4ac7e37afac4590c3488f87a9719fbb8b5530f6c
Korumilli Venkata Rama Krishna Varma <ramakrishnavarma.korumilli@indiumsoft.com>
2022-10-25 16:00:47 +0530

    api integration

--- crud/src/CRUD.js
+++ crud/src/CRUD.js
@@ -1,41 +1,43 @@
-import React, { useRef, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import './CRUD.css'
+
 function CRUD() {
-    const list = [
-        {
-            id: 1,
-            name: "HP",
-            price: "2222"
-        },
-        {
-            id: 2,
-            name: "Dell",
-            price: "2445"
-        },
-    ]
-    const [lists, setList] = useState(list)
+    const [lists, setList] = useState([])
+    const [update, setUpdate] = useState(false)
     const [updateState, setUpdateState] = useState(-1)
-    return(
-    <div className='crud'>
-        <div>
-        <AddList setList = {setList }/>
-        <form onSubmit={handleSubmit}>
-            <table>
-                {
-                    lists.map((current) => (
-                        updateState === current.id ? <EditList current={current} lists={lists} setList={setList}/> :
-                        <tr>
-                            <td>{current.name}</td>
-                            <td>{current.price}</td>
-                            <td>
-                                <button className='edit' onClick={() => handleEdit(current.id)}>Edit</button>
-                                <button className='delete' type='button' onClick={() =>handleDelete(current.id)}>Delete</button>
-                            </td>
-                        </tr>
-                    ))
-                }
-            </table>
-            </form>
+
+
+    useEffect(() => {
+        console.log('test')
+        fetch('https://testdemo987.herokuapp.com/todos').then(res => {
+            return res.json()
+        }).then(data => {
+            setList(data)
+        })
+    })
+
+
+    return (
+        <div className='crud'>
+            <div>
+                <AddList setList={setList} lists={lists} update={update} setUpdate={setUpdate}/>
+                <form onSubmit={handleSubmit}>
+                    <table>
+                        {
+                            lists.map((current) => {
+                                    return <tr>
+                                        <td>{current.name}</td>
+                                        <td>{current.description}</td>
+                                        <td>
+                                            <button className='edit' onClick={() => handleEdit(current.id)}>Edit</button>
+                                            <button className='delete' type='button' onClick={() => handleDelete(current.id)}>Delete</button>
+                                        </td>
+                                    </tr>
+                            }
+                            )
+                        }
+                    </table>
+                </form>
             </div>
         </div>
     )
@@ -52,63 +54,76 @@ function CRUD() {
         const name = event.target.elements.name.value
         const price = event.target.elements.price.value
         const newlist = lists.map((li) => (
-            li.id === updateState ? {...li, name:name, price: price} : li
+            li.id === updateState ? { ...li, name: name, price: price } : li
         ))
-        
+
         setList(newlist)
         setUpdateState(-1)
     }
 }
 
-function EditList({current, lists, setList}) {
+function EditList({ current, lists, setList }) {
     function handInputname(event) {
         const value = event.target.value;
         const newlist = lists.map((li) => (
-            li.id === current.id ? {...li, name:value} : li
+            li.id === current.id ? { ...li, name: value } : li
         ))
-        
+
         setList(newlist)
     }
     function handInputprice(event) {
         const value = event.target.value;
         const newlist = lists.map((li) => (
-            li.id === current.id ? {...li, price :value} : li
+            li.id === current.id ? { ...li, price: value } : li
         ))
-        
+
         setList(newlist)
     }
-    return(
+    return (
         <tr>
-            <td><input type="text" onChange={handInputname} name='name' value={current.name}/></td>
-            <td><input type="text" onChange={handInputprice} name='price' value={current.price}/></td>
+            <td><input type="text" onChange={handInputname} name='name' value={current.name} /></td>
+            <td><input type="text" onChange={handInputprice} name='price' value={current.price} /></td>
             <td><button type='submit'>Update</button></td>
         </tr>
     )
 }
 
-function AddList({setList}) {
+function AddList({ setList, lists, update, setUpdate}) {
     const nameRef = useRef()
     const priceRef = useRef()
 
     function handleSubmit(event) {
         event.preventDefault();
         const name = event.target.elements.name.value;
-        const price = event.target.elements.price.value;
+        const description = event.target.elements.price.value;
         const newlist = {
-            id: 3,
             name,
-            price
+            description
         }
-        setList((prevList)=> {
-            return prevList.concat(newlist)
+        let req = {
+            method: 'POST',
+            headers:{
+                'Content-Type': 'application/json'
+            },
+            body:JSON.stringify(newlist)
+        }
+        fetch('https://testdemo987.herokuapp.com/todos',req).then(res => {
+            return res.json()
+        }).then(data => {
+            console.log(lists)
+            lists.unshift(data)
+            console.log(lists)
+            setList(lists)
+            setUpdate(!update)
         })
+
         nameRef.current.value = ""
-        priceRef.current.value = ""      
+        priceRef.current.value = ""
     }
-    return(
+    return (
         <form className='addForm' onSubmit={handleSubmit}>
-            <input type="text" name="name" placeholder="Enter Name" ref={nameRef}/>
-            <input type="text" name="price" placeholder="Enter Price" ref={priceRef}/>
+            <input type="text" name="name" placeholder="Enter Name" ref={nameRef} />
+            <input type="text" name="price" placeholder="Enter Description" ref={priceRef} />
             <button type="submit">Add</button>
         </form>
     )
